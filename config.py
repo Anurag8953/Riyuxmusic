5546bd61077ba604a86a8fb1d73dd3c8b78f58ee
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2023-11-22 21:05:58 -0800

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Venomxboss/KunoMusic",
+    "https://github.com/aashuxxD/KunoMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
