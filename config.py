f584c28376c261d3799580b59784cee5aba3c3ae
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2023-12-26 12:34:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/aashuxxD/KunoMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "test")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
